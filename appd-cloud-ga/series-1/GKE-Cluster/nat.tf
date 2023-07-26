510533943509e6a74112303800c444867874a335
charleslin-appd <charles.lin@appdynamics.com>
2023-07-25 19:50:16 -0600

    Add firewall to allow api to talk to otel webhook which listens at 9443

--- appd-cloud-ga/series-1/GKE-Cluster/nat.tf
+++ appd-cloud-ga/series-1/GKE-Cluster/nat.tf
@@ -22,3 +22,19 @@ resource "google_compute_router_nat" "nat" {
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
+
+resource "google_compute_firewall" "gke-opentelemetry-webhook" {
+  project     = var.project_id
+  name        = "gke-opentelemetry-webhook-rule"
+  network     = "k8s-network"
+  description = "Creates firewall rule to allow control plane api server to talk to opentelemetry webhook which uses port 9443"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9443"]
+  }
+
+  source_ranges = ["172.16.0.16/28"]
+  #target_tags   = ["gke-k8s-cluster-pods-6ab58d08-node"]
+  target_tags = [var.gke_cluster_name]
+}
